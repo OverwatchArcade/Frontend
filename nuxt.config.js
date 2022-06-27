e94625991df2175309ceb641e6b26994d50c9bab
Quinten Peels <quinten@peels.me>
2022-06-27 22:28:45 +0200

    css fontawesome test

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/style'],
+  css: ['~/assets/style', '@fortawesome/fontawesome-svg-core/styles.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
