f10b7d403c93764412d8753b9b5df68eb826abac
Quinten Peels <quinten@peels.me>
2022-07-01 13:29:35 +0200

    Revert "test - trying ssr to fix browser capture"
    This reverts commit abe10af1c4d3673ebce9181b9ab77b90c9a9190c.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-  target: 'server',
+  target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Overwatch arcade gamemodes - daily updated and get notified easily',
